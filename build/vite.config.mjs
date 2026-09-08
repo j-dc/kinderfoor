769a79325602d2615fc29781ac9816e13b280463
Jan Declercq <jan.declercq@gmail.com>
2026-09-08 23:27:35 +0200

    more html pages

--- build/vite.config.mjs
+++ build/vite.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   root: 'src',
@@ -9,6 +10,12 @@ export default defineConfig({
   },
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve('src/index.html'),
+        'kinderfoor-2028': resolve('src/kinderfoor-2028.html')
+      }
+    }
   }
 });
